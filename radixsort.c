3d92268aa1847ac12162f6ca6209ef2e38800a1c
Vineet Dhanawat <vineetdhanawat@gmail.com>
2014-07-31 21:11:50 -0700

    Updated radixsort

--- radixsort.c
+++ radixsort.c
@@ -4,48 +4,50 @@
 */
 
 #include <stdio.h>
-int array[] = {0};
-#define max 10
 #define base 10
-void radixSort(int array[])
+
+void radixSort(int array[], int length)
 {
-	int i,largest=array[0];
-	int temp[max];
-	for(i=0;i<max;i++)
+	int bucket[base]={0};
+	int temp[length];
+	int i,largest=0;
+	for(i=0;i<length;i++)
 	{
 		if(array[i]>largest)
-		largest=array[i];
+		largest = array[i];
 	}
 	
 	int arg=1;
-	while(largest/arg > 0)
+	while(largest/arg>0)
 	{
-		// number of bases
+		// IMP: reset bucket everytime
 		int bucket[base]={0};
-		for(i=0;i<max;i++)
+		for(i=0;i<length;i++)
 			bucket[(array[i]/arg)%base]++;
-		
-		for(i=1;i<max;i++)
-			bucket[i]+=bucket[i-1];
 
-		for(i=0;i<max;i++)
+		for(i=1;i<base;i++)
+			bucket[i] += bucket[i-1];
+		
+		for(i=length-1;i>=0;i--)
 			temp[--bucket[(array[i]/arg)%base]] = array[i];
-			
-		for(i=0;i<max;i++)
+		
+		for(i=0;i<length;i++)
 			array[i] = temp[i];
-			
-		arg *= 10;
+		
+		arg*=10;
 	}
 }
+
 void main()
 {
-	int i;
-	printf("Enter integers:\n");
-	for(i=0;i<max;i++)
-	scanf("%d",&array[i]);
+	int array[] = {535,44,423};
+	int length = sizeof(array)/sizeof(array[0]);
+	int k;
 	
-	radixSort(array);
+	radixSort(array,length);
 	
-	for(i=0;i<max;i++)
-	printf("%d ",array[i]);
+	for(k=0;k<length;k++)
+	{
+		printf("%d ",array[k]);
+	}
 }
